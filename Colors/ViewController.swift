b8aa1ef5ed2c4f23496430677d9a777fe9a142c2
Cristina S. Barreiro <cristina.sbarreiro@gmail.com>
2017-08-19 16:36:09 +0200

    Hide status bar. Add button functionality

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSwitch: UIButton!
+    @IBOutlet weak var imgKnobBase: UIImageView!
+    @IBOutlet weak var imgKnob: UIImageView!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imgKnob.isHidden = true
+        imgKnobBase.isHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_off"), for: .normal)
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_on"), for: .selected)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnSwitchPressed(_ sender: UIButton) {
+        btnSwitch.isSelected = !btnSwitch.isSelected
+        if (btnSwitch.isSelected) {
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0.5, brightness: 0.75, alpha: 1.0)
+            imgKnobBase.isHidden = false
+            imgKnob.isHidden = false
+        } else {
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0, brightness: 0.2, alpha: 1.0)
+            imgKnobBase.isHidden = true
+            imgKnob.isHidden = true
+        }
     }
 
-
+    
 }
 
